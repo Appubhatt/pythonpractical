18610708397abc3b259bb977cb0606637e44db4d
Appubhatt <47659350+Appubhatt@users.noreply.github.com>
2022-02-01 23:05:11 +0530

    Add files via upload

---
+++ Prac5.py
@@ -0,0 +1,8 @@
+# You are given a string and your task is to swap cases.
+# In other words, convert all lowercase letters to uppercase letters and vice versa.
+# Sample Input: HackerRank.com presents "Pythonist 2".
+# Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2".
+a=input()
+print(a.swapcase())
+print('''Created by: Apurva Bhatt
+Id:D21ce173''')
